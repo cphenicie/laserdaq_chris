a27d3f9c85aa314dd5b74717bab90d5b815bd685
Christopher Phenicie <phenicie@princeton.edu>
2016-09-09 09:30:59 -0400

    Created

---
+++ kjlc615_server.py
@@ -0,0 +1,28 @@
+from labrad import types as T, util
+from labrad.server import LabradServer, setting
+from twisted.internet.defer import inlineCallbacks, returnValue
+import serial
+
+class KJLC615(LabradServer):
+    name = 'kjlc615'  # This is how you will access this class from a labrad connection (with name.replace(" ", "_") and name.lower() )
+
+    @inlineCallbacks
+    def initServer(self):
+        self.ser = serial.Serial('COM3', 9600, timeout=0, parity=serial.PARITY_NONE, rtscts=1)
+        yield None
+
+
+    @setting(1,'echo',msg='?',returns='?')
+    def echo(self,c,msg):
+        print(msg)
+        return msg
+
+    @setting(2, 'readPressure', msg='?', returns='?')
+    def readPressure(self):
+        pressStr = self.ser.readline()
+        return T.Value(pressStr[1:4], 'Torr')
+
+__server__ = KJLC615()
+
+if __name__ == '__main__':
+    util.runServer(__server__)
